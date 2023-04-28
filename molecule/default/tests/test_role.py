7f5f338a3002f5e8ff11a900671fc1957a8e049d
Nicolas Boutet <amd3002@gmail.com>
2023-04-28 12:37:55 +0200

    Adapt CI workflow for molecule 5

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -5,12 +5,16 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('file,user,group,mode', [
-  ('/tmp/mariadb-java-client-3.1.3.jar', 'root', 'root', 0o644),
-])
+@pytest.mark.parametrize(
+    "file,user,group,mode",
+    [
+        ("/tmp/mariadb-java-client-3.1.3.jar", "root", "root", 0o644),
+    ],
+)
 def test_connector_is_installed(host, file, user, group, mode):
     connector = host.file(file)
     assert connector.exists
