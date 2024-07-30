11a8d14e290d07b897c3e9d998e9aaa2a9285073
Nicolas Boutet <amd3002@gmail.com>
2024-07-30 11:29:43 +0200

    Update to MariaDB Connector/J 3.4.1

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.mark.parametrize(
     "file,user,group,mode",
     [
-        ("/tmp/mariadb-java-client-3.4.0.jar", "root", "root", 0o644),
+        ("/tmp/mariadb-java-client-3.4.1.jar", "root", "root", 0o644),
     ],
 )
 def test_connector_is_installed(host, file, user, group, mode):
